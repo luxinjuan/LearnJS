660083475de8b717cc1ef34748ce372a4d98de4b
luxinjuan <1769099582@qq.com>
2020-06-09 17:36:02 +0800

    609change

--- tool.js
+++ tool.js
@@ -1,5 +1,5 @@
+ //获取node这个节点下所有的子节点
  function elementByClassName(node,classStr){
-     //1.获取node这个节点下所有的子节点
      var nodes = node.getElementsByTagName("*");  //*代表通配符
      var arr = [];//存放符合条件的节点
      for(var i = 0;i<nodes.length;i++){
@@ -9,13 +9,53 @@
      }
      return arr;
  }
+
+ //随机颜色设置
+function randomColor(){
+    var str ="rgba(" +parseInt(Math.random() *256)+","+parseInt(Math.random() *256)+","+parseInt(Math.random() *256);
+    return str;
+ }
+ 
  
  //跨浏览器兼容
  function getStyle(node,cssStyle){
     return node.currentStyle ? node.currentStyle[cssStyle]:getComputedStyle(node)[cssStyle];
 }
-//随机颜色设置
-function randomColor(){
-   var str ="rgba(" +parseInt(Math.random() *256)+","+parseInt(Math.random() *256)+","+parseInt(Math.random() *256);
-   return str;
+
+ //封装跨浏览器兼容的阻止事件冒泡
+ function stopBubble(e){
+    //e是事件对象
+    if(e.stopPropagation()){
+        e.stopPropagation()
+    }else{
+        e.cancelBubble = true;
+    }
+}
+
+  //编写一个跨浏览器阻止超链接默认行为的函数
+  function perDef(e){
+    if(e.preventDefault){
+        e.preventDefault();
+    }else{
+        window.event.returnValue = false;
+    }
+}
+
+/*
+    事件监听器的兼容
+    attachEvent()和detachEvent()
+*/
+function addEvent(node,eventType,funcName){
+    if(node.addEventListener){
+        node.addEventListener(eventType,funcName,false)
+    }else{
+        node.attachEvent("on"+eventType,funcName);
+    }
+}
+function removeEvent(node,eventType,funcName){
+    if(node.removeEventListener){
+        node.removeEventListener(eventType,funcName)
+    }else{
+        node.和detachEvent("on"+eventType,funcName);
+    }
 }
